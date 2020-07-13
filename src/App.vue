ce5c9670ed9f5ab950ae1a26be87901375e173ff
lucasmss16 <lucasmss16@gmail.com>
2020-07-12 22:12:03 -0300

    Estrututa basica da tela com dados estaticos

--- src/App.vue
+++ src/App.vue
@@ -2,64 +2,74 @@
 <div>
   <b-navbar type="dark" variant="dark">
     <b-navbar-nav>
-      <b-nav-item href="#" class="textP">PAINEL COVID - 19</b-nav-item>
 
+      <b-nav-item href="#" class="textP">PAINEL COVID - 19</b-nav-item>
       <b-nav-item href="#" class="textPsimples">Painel Geral</b-nav-item>
 
-      <b-nav-item-dropdown text="Sobre" class="textPsimples" >
+      <b-nav-item-dropdown text="Sobre" class="textPsimplesSobre" >
         <b-dropdown-item href="#">Nós</b-dropdown-item>
         <b-dropdown-item href="#">Sistema</b-dropdown-item>
         <b-dropdown-item href="#">Contato</b-dropdown-item>
       </b-nav-item-dropdown>
+
     </b-navbar-nav>
   </b-navbar>
 
    <div class="dadosMarg  container">
-    <b-table :striped="striped"
-      :bordered="bordered"
-      :borderless="borderless"
-      :outlined="outlined"
-      :small="small"
-      :hover="hover"
-      :dark="dark"
-      :fixed="fixed"
-      :foot-clone="footClone"
-      :no-border-collapse="noCollapse"
-      :items="items"
-      :fields="fields"
-      :head-variant="headVariant"
-      :table-variant="tableVariant"></b-table>
-  </div>
 
-<b-container fluid="md">
-    <h1 class="h1 dadosMargTabela ">Casos confirmados e acumulados</h1>
-  <div class="row">
-    <line-chart class="col-sm-6" :data="chartData"></line-chart>
-    <bar-chart class="col-sm-6" :data="chartData"></bar-chart>
-  </div>
+      <b-dropdown split text="Selecionar Região" class="m-6">
+        <b-dropdown-item href="#">Centro-Oeste</b-dropdown-item>
+        <b-dropdown-item href="#">Sul</b-dropdown-item>
+        <b-dropdown-item href="#">Norte</b-dropdown-item>
+      </b-dropdown>
+    
+      <b-table :striped="striped"
+        :bordered="bordered"
+        :borderless="borderless"
+        :outlined="outlined"
+        :small="small"
+        :hover="hover"
+        :dark="dark"
+        :fixed="fixed"
+        :foot-clone="footClone"
+        :no-border-collapse="noCollapse"
+        :items="items"
+        :fields="fields"
+        :head-variant="headVariant"
+        :table-variant="tableVariant">
+      </b-table>
 
-<h1 class="h1 dadosMargTabela ">Novos casos no dia</h1>
-  <div class="row">
-    <line-chart class="col-sm-6" :data="chartData"></line-chart>
-    <line-chart class="col-sm-6" :data="chartData"></line-chart>
-  </div>
+    </div>
 
-<h1 class="h1 dadosMargTabela ">Óbitos acumulados</h1>
-  <div class="row">
-    <line-chart class="col-sm-6" :data="chartData"></line-chart>
-    <line-chart class="col-sm-6" :data="chartData"></line-chart>
-  </div>
+  <b-container fluid="md">
+      <h1 class="h1 dadosMargTabela ">Casos confirmados e acumulados</h1>
+        <div class="row">
+          <line-chart class="col-sm-6" :data="chartData"></line-chart>
+          <bar-chart class="col-sm-6" :data="chartData"></bar-chart>
+        </div>
 
-<h1 class="h1 dadosMargTabela ">Óbitos no dia</h1>
-  <div class="row">
-    <line-chart class="col-sm-6" :data="chartData"></line-chart>
-    <line-chart class="col-sm-6" :data="chartData"></line-chart>
-  </div>
+      <h1 class="h1 dadosMargTabela ">Novos casos no dia</h1>
+        <div class="row">
+          <line-chart class="col-sm-6" :data="chartData"></line-chart>
+          <line-chart class="col-sm-6" :data="chartData"></line-chart>
+        </div>
+
+      <h1 class="h1 dadosMargTabela ">Óbitos acumulados</h1>
+        <div class="row">
+          <line-chart class="col-sm-6" :data="chartData"></line-chart>
+          <line-chart class="col-sm-6" :data="chartData"></line-chart>
+        </div>
+
+      <h1 class="h1 dadosMargTabela ">Óbitos no dia</h1>
+        <div class="row">
+          <line-chart class="col-sm-6" :data="chartData"></line-chart>
+          <line-chart class="col-sm-6" :data="chartData"></line-chart>
+        </div>
   </b-container>
 
-<footer class="footer">
-  <p class="textFotter">Desenvolvido por Lucas Mateus Silva</p>
-</footer>
+  <footer class="footer">
+    <p class="textFotter">Desenvolvido por Lucas Mateus Silva</p>
+  </footer>
 
 </div>
 </template>
@@ -70,7 +80,7 @@ export default {
       return {
         fields: ['Estado', 'Casos', 'Óbitos', 'Atualização'],
         items: [
-          { Estado: 'Brasilia', Casos: '144.804' , Óbitos: '2.897', Atualização: '11/07 19:00'},
+          { Estado: 'Centro-Oeste', Casos: '144.804' , Óbitos: '2.897', Atualização: '11/07 19:00'},
           { Estado: 'Sul', Casos: '121.543', Óbitos: '2.444', Atualização: '11/07 19:00' },
           { Estado: 'Norte', Casos: '318.807', Óbitos: '10.453', Atualização: '11/07 19:00' }
         ],
@@ -130,6 +140,11 @@ export default {
   color: white;
 }
 .textPsimples {
+  font-size: 23px;
+  padding-left: 870px;
+  color: white;
+}
+.textPsimplesSobre {
   font-size: 23px;
   color: white;
 }
